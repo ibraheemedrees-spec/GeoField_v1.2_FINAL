3b22ae08cacf52f5bb63e0e1dd24af8c97b0053d
ibraheemedrees-spec <ibraheemedrees2010@gmail.com>
2026-08-11 04:24:44 -0700

    Delete src directory

--- src/devices/IDevice.cpp
+++
@@ -1,5 +0,0 @@
-#include "IDevice.h"
-
-// Implementation is header-only for the constructor.
-// This translation unit exists so AUTOMOC generates and links
-// moc_IDevice.cpp (meta-object for signals / Q_ENUM).
